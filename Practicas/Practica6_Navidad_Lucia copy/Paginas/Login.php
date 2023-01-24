d4a9696309421103fd6352b29f56d97617cc607e
Lucia <lucia.matest@educa.jcyl.es>
2023-01-24 22:01:26 +0000

    1

--- Practicas/Practica6_Navidad_Lucia copy/Paginas/Login.php
+++ Practicas/Practica6_Navidad_Lucia copy/Paginas/Login.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="../CSS/style.css">
     <title>Login - Bootstrap</title>
 </head>
 
@@ -20,7 +20,7 @@
         <div class="mx-2">
             <div class="d-flex bd-highlight mb-1 align-items-center justify-content-space-between">
                 <div class="me-auto mb-1 bd-highlight" class="logo">
-                    <img src="./Imagen/style.css" alt="logo" class="icono_logo" style="width: 200px;">
+                    <img src="../Imagen/logo.png" alt="logo" class="icono_logo" style="width: 200px;">
                 </div>
 
                 <div class="p-2">
@@ -62,39 +62,32 @@
     </nav>
 
     <main>
-        <div class="login_form_wrapper">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
-                        <!-- login_wrapper -->
-                        <div class="login_wrapper">
-                            <div class="formsix-pos">
-                                <div class="form-group i-email">
-                                    <input type="email" class="form-control" required="" id="email2" placeholder="Email Address *">
-                                </div>
-                            </div>
-                            <div class="formsix-e">
-                                <div class="form-group i-password">
-                                    <input type="password" class="form-control" required="" id="password2" placeholder="Password *">
-                                </div>
-                            </div>
-                            <div class="login_remember_box">
-                                <label class="control control--checkbox">Remember me
-                                    <input type="checkbox">
-                                    <span class="control__indicator"></span>
-                                </label>
-                                <a href="#" class="forget_password">
-                                    Forgot Password
-                                </a>
-                            </div>
-                            <div class="login_btn_wrapper">
-                                <a href="#" class="btn btn-primary login_btn"> Login </a>
-                            </div>
-                            <div class="login_message">
-                                <p>Don&rsquo;t have an account ? <a href="#"> Sign up </a> </p>
-                            </div>
+        <div class="container">
+            <div class="row justify-content-center mt-5">
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="card">
+                        <div class="card-title text-center">
+                            <h2 class="p-3">Acceder</h2>
                         </div>
-                        <!-- /.login_wrapper-->
+                        <div class="card-body">
+                            <form>
+                                <div class="mb-4">
+                                    <label for="username" class="form-label">Username/Email</label>
+                                    <input type="text" class="form-control" id="username" />
+                                </div>
+                                <div class="mb-4">
+                                    <label for="password" class="form-label">Password</label>
+                                    <input type="password" class="form-control" id="password" />
+                                </div>
+                                <!-- <div class="mb-4">
+                                    <input type="checkbox" class="form-check-input" id="remember" />
+                                    <label for="remember" class="form-label">Remember Me</label>
+                                </div> -->
+                                <div class="d-grid">
+                                    <button type="submit" class="btn text-light main-bg">Login</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
