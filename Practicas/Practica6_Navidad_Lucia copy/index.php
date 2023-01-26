c9f791a3eb5c202ea63fe4b4c5e205d1244f8921
Lucia <lucia.matest@educa.jcyl.es>
2023-01-26 20:53:55 +0000

    1

--- Practicas/Practica6_Navidad_Lucia copy/index.php
+++ Practicas/Practica6_Navidad_Lucia copy/index.php
@@ -20,8 +20,8 @@
     <header class="p-3" style="background-color: #f0f0f0;">
         <div class="mx-2">
             <div class="d-flex bd-highlight mb-1 align-items-center justify-content-space-between">
-                <a href="index.php" class="me-auto mb-1 bd-highlight" class="logo">
-                    <img src="Imagen/logo.png" alt="logo" class="pe-sm-5" style="width: 200px;">
+                <a href="index.php" class="mx-auto mx-sm-auto ms-md-0 my-1" class="logo">
+                    <img src="Imagen/logo.png" alt="logo" class="icono_logo" style="width: 200px;">
                 </a>
 
                 <div class="p-2 p-sm-0">
