01bb45f07826147c08ff72d5d5d05811cf8d798a
Lucia <lucia.matest@educa.jcyl.es>
2023-02-01 10:40:57 +0000

    1

--- Temas/Tema 8/3/js/query.js
+++ Temas/Tema 8/3/js/query.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('#balon').click(function () {
       $(this).css("background-color", "red");
-      $(this).animate({top: '10%'});
+      $(this).animate({top: '10%'}, function(){
+        $('#balon').css("background-color", "green");
+      });
     })
   });
---
+++ Temas/Tema 8/4/js/query.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("#enviar").click(function(){
+    alert("Text: " + $("#test").text());
+  });
+})
