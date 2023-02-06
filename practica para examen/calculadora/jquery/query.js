40b440a3ee19f0b6350ce89592d36d7d94a334c1
Lucia <lucia.matest@educa.jcyl.es>
2023-02-06 19:04:34 +0000

    1

---
+++ practica para examen/calculadora/jquery/query.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('#uno').click(function () {
+        $('#resultado').append('1');
+    });
+    $('#dos').click(function () {
+        $('#resultado').append('2');
+    });
+    $('#tres').click(function () {
+        $('#resultado').append('3');
+    });
+    $('#cuatro').click(function () {
+        $('#resultado').append('4');
+    });
+    $('#cinco').click(function () {
+        $('#resultado').append('5');
+    });
+    $('#seis').click(function () {
+        $('#resultado').append('6');
+    });
+    $('#siete').click(function () {
+        $('#resultado').append('7');
+    });
+    $('#ocho').click(function () {
+        $('#resultado').append('8');
+    });
+    $('#nueve').click(function () {
+        $('#resultado').append('9');
+    });
+    $('#cero').click(function () {
+        $('#resultado').append('0');
+    });
+});
