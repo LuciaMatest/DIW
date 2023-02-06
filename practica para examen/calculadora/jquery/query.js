d2c727db42024f2cc23371d7594becc6ea4223da
Lucia <lucia.matest@educa.jcyl.es>
2023-02-06 19:47:05 +0000

    1

--- practica para examen/calculadora/jquery/query.js
+++ practica para examen/calculadora/jquery/query.js
@@ -1,7 +1,3 @@
-let operandoa;
-let operandob;
-let operacion;
-
 $(document).ready(function(){
     $('#uno').click(function () {
         $('#resultado').append('1');
@@ -35,38 +31,18 @@ $(document).ready(function(){
     });
     $('#suma').click(function () {
         
-    })
+    });
+    $('#resta').click(function () {
+        
+    });
+    $('#multiplica').click(function () {
+        
+    });
+    $('#divide').click(function () {
+        
+    });
+    $('#igual').click(function () {
+        
+    });
+    
 });
-
-// $(document).ready(function(){
-//     $('#uno').on("click", function(){
-//         $('#resultado').append('1');
-//       });
-//     $('#dos').on("click", function(){
-//         $('#resultado').append('2');
-//     });
-//     $('#tres').on("click", function(){
-//         $('#resultado').append('3');
-//     });
-//     $('#cuatro').on("click", function(){
-//         $('#resultado').append('4');
-//     });
-//     $('#cinco').on("click", function(){
-//         $('#resultado').append('5');
-//     });
-//     $('#seis').on("click", function(){
-//         $('#resultado').append('6');
-//     });
-//     $('#siete').on("click", function(){
-//         $('#resultado').append('7');
-//     });
-//     $('#ocho').on("click", function(){
-//         $('#resultado').append('8');
-//     });
-//     $('#nueve').on("click", function(){
-//         $('#resultado').append('9');
-//     });
-//     $('#cero').on("click", function(){
-//         $('#resultado').append('0');
-//     });
-// });
