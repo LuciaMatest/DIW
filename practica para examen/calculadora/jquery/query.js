3d54d5a84c2c7089991de85687e8ef25aaa2c106
Lucia <lucia.matest@educa.jcyl.es>
2023-02-06 20:25:15 +0000

    1

--- practica para examen/calculadora/jquery/query.js
+++ practica para examen/calculadora/jquery/query.js
@@ -1,12 +1,15 @@
 
 $("button").click(function() {
-    valor = $(this).val();
-    actual = $("#resultado").val();
+    //variable valor del boton pulsado
+    let valor = $(this).val();
+    //variable valor de la pantalla
+    let actual = $("#resultado").val();
+    //Si se pulsa C reseteamos la pantalla
     if (valor == "C") {
       $("#resultado").val("");
     } else {
       if (valor == "=") {
-        $("#resultado").val(eval(actual));
+        $("#resultado").val(eval(valor));
       } else {
         $("#resultado").val(actual + valor);
       }
