b33208c693aa43372bd780d7ffc7d457ad869342
Lucia <lucia.matest@educa.jcyl.es>
2023-02-06 21:14:40 +0000

    1

--- practica para examen/formulario/jquery/query.js
+++ practica para examen/formulario/jquery/query.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('button').click(function(evento){
         evento.preventDefault();
-        alert(`Bienvenido ${$('#idNombre').val()} ${$('#idApellido').val()}. Se te ha generado un nuevo correo electronico con la dirección ${$('#idNombre').val()} ${$('#idApellido').val()}1@diw.com`)
+        alert(`Bienvenido ${$('#idNombre').val()} ${$('#idApellido').val()}. 
+Se te ha generado un nuevo correo electronico con la dirección ${$('#idNombre').val()} ${$('#idApellido').val()}1@diw.com`)
     })
 })
