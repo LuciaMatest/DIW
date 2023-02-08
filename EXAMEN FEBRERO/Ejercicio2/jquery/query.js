d6d59c248d13e483184d69928aad156e8b479677
Lucia <lucia.matest@educa.jcyl.es>
2023-02-08 11:39:14 +0000

    1

--- EXAMEN FEBRERO/Ejercicio2/jquery/query.js
+++ EXAMEN FEBRERO/Ejercicio2/jquery/query.js
@@ -3,3 +3,49 @@ $(document).ready(function () {
         $('#submenu').slideToggle("slow");
     });
 });
+
+// Funciones de video
+//Play
+function iniciarVideo() {
+    let botonPlay = document.getElementById('botonPlay');
+    botonPlay.addEventListener('click', presionarPlay);
+}
+
+function presionarPlay() {
+    let video = document.getElementById('videoGato');
+    video.playbackRate = 1.0;
+    video.play();
+}
+
+//Pausar
+function pausarVideo() {
+    let botonPause = document.getElementById('botonPause');
+    botonPause.addEventListener('click', presionarPause);
+}
+
+function presionarPause() {
+    let video = document.getElementById('videoGato');
+    video.pause();
+}
+
+//Silenciar
+function silenciarVideo() {
+    let botonSilenciar= document.getElementById('botonSilenciar');
+    botonSilenciar.addEventListener('click', presionarSilenciar);
+}
+
+function presionarSilenciar() {
+    let video = document.getElementById('videoGato');
+    video.muted = true;
+}
+
+//Sonido
+function sonidoVideo() {
+    let botonSonido= document.getElementById('botonSonido');
+    botonSonido.addEventListener('click', presionarSonido);
+}
+
+function presionarSonido() {
+    let video = document.getElementById('videoGato');
+    video.muted = false;
+}
